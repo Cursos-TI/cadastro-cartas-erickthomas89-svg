dbc94adbd335bd2406217165486c53f3771b8db8
erickthomas89-svg <erickthomas89@gmail.com>
2026-03-11 20:37:50 -0300

    Adicionei os dados na carta de Densidade Populacional e PIB per capita
    Primeiro passo: Adicionar variáveis
    Variável para armazenar a densidade populacional
    Variável para armazenar o PIB per capita
    float densidade_populacional;
    float pib_per_capita;
    
    Segundo passo: Montar os cálculos 
    Cálculo da densidade populacional em hab/km²
    Convertendo PIB para reais antes de calcular o PIB per capita
    densidade_populacional = populacao / area;
     pib_per_capita = (pib * 1000000) / populacao;
    
    Terceiro passo: Adicionar na saída de dados
    printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional);
    printf("PIB per Capita: %.2f reais\n", pib_per_capita);
    
    Rodei e funcionou tudo certo.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,60 +3,71 @@
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
+// Objetivo: Adicionar e Calcular a Densisade Populacional e o PIB per capita
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
-  char codigo_carta[4];
   char estado;
-  char cidade[50];
+  char codigo_carta[4]; // Exemplo: 1, 2, etc.
+  char cidade[50]; // Nome da cidade
   int populacao;
   float area;
   float pib;
-  int pontos_turisticos;
-  char continuar;  // variavel para controlar o loop de cadastro
+  int pontos_turisticos; 
+  float densidade_populacional; //adicionai a variável para densidade em float
+  float pib_per_capita; //adicionei a variável pib per capita também em float
+  char continuar; // Variável para controlar o loop de cadastro 
+
 
   // Área para entrada de dados
   do {
-    printf("=== Cadastro de Carta de Super Trunfo ===\n");
+  printf("=== Cadastro de Carta de Super Trunfo ===\n");
 
-    printf("Digite o código da carta (ex.: 1): ");
-    scanf("%s", codigo_carta);
+  printf("Digite o código da carta (ex: 1): "); 
+  scanf("%s", &codigo_carta);
 
-    printf("Digite o estado (A-H): ");
-    scanf(" %c", &estado);
+  printf("Digite o estado (A-H): ");
+  scanf(" %c", &estado);
 
-    printf("Nome da cidade: ");
-    scanf("%s", cidade);
+  printf("Nome da cidade: ");
+  scanf("%s", &cidade);
 
-    printf("Digite a área (em km²): ");
-    scanf("%f", &area);
+  printf("Digite a população: ");
+  scanf("%d", &populacao);
 
-    printf("Digite o PIB (em milhões): ");
-    scanf("%f", &pib);
+  printf("Digite a área (em km²): ");
+  scanf("%f", &area);
 
-    printf("Digite o número da pontos turísticos: ");
-    scanf("%d", &pontos_turisticos);
+  printf("Digite o PIB (em milhões): ");  
+  scanf("%f", &pib);
+  
+  printf("Digite o número de pontos turísticos: ");
+  scanf("%d", &pontos_turisticos); 
+  
+  densidade_populacional = populacao / area;
+  pib_per_capita = (pib * 1000000) / populacao; 
+  // Converti PIB para reais antes de calcular o PIB per capita para o progamar dar saída no formato correto.
 
-    // Área para exibição dos dados da cidade
-    printf("\n=== Carta de Super trunfo cadastrada ===\n");
-    printf("Estado: %c\n", estado);
-    printf("Código da Carta: %s\n", codigo_carta);
-    printf("Cidade: %s\n", cidade);
-    printf("População: %d\n", populacao);
-    printf("Área: %.2f km²\n", area);
-    printf("PIB: %.2f milhões\n", pib);
-    printf("Pontos Turísticos: %d\n", pontos_turisticos);
+  // Área saída de dados
+  printf("\n=== Carta de Super Trunfo Cadastrada ===\n"); 
+  printf("Código da Carta: %s\n", codigo_carta);
+  printf("Estado: %c\n", estado);
+  printf("Cidade: %s\n", cidade);
+  printf("População: %d\n", populacao); 
+  printf("Área: %.2f km²\n", area);
+  printf("PIB: %.2f milhões\n", pib);
+  printf("Pontos Turísticos: %d\n", pontos_turisticos);
+  printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional); //Cálculo da densidade populacional em hab/km²
+  printf("PIB per Capita: %.2f reais\n", pib_per_capita); // Cálculo do PIB per capita em reais
 
-    printf("Deseja cadastrar a próxima carta ? (S/N): ");
-    scanf("%c", &continuar);
-    printf("\n");
+  printf("Deseja cadastrar o próximo? (S/N): ");
+  scanf(" %c", &continuar);
+  printf("\n");
 
-  } while (continuar == 'S' || continuar == 's'); //Loop para cadastrar múltiplas cartas
+  } while (continuar == 'S' || continuar == 's'); // Loop para cadastrar múltiplas cartas 
 
   printf("Cadastro de cartas concluído. Obrigado por participar!\n");
   
-    return 0;
+  return 0;
+  
 
-
-  }
+} 
